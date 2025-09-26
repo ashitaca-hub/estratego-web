c92b5caa18d30899b78c2c7d41bdccbb73bc81aa
unknown <Administrador@PC121.tercat.local>
2025-09-26 12:33:49 +0200

    Reestructura: mueve todos los archivos Next.js a /frontend

--- .next/server/app/api/prematch/route_client-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
-globalThis.__RSC_MANIFEST["/api/prematch/route"] = {"moduleLoading":{"prefix":"","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
